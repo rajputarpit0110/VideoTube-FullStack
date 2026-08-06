d5ddd8dc76af52e231633013dd1d1763aae5e28f
Arpit Rajput <arpitrajput@Arpits-MacBook-Air.local>
2026-08-06 19:54:11 +0530

    Update options

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -174,7 +174,8 @@ const logoutUser = asyncHandler(async(req, res) => {
 
     const options = {
         httpOnly: true,
-        secure: true
+        secure: true,
+        sameSite: "none",
     }
 
     return res
@@ -210,7 +211,8 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     
         const options = {
             httpOnly: true,
-            secure: true
+            secure: true,
+            sameSite: "none",
         }
     
         const {accessToken, newRefreshToken} = await generateAccessAndRefereshTokens(user._id)
